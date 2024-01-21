6c2bd5f6d3626b4435a8cf8a10c50e2f4e08f6ce
tulobaev <talgattulobaev519>
2024-01-22 02:35:09 +0600

    changes file

--- src/Main.js
+++ src/Main.js
@@ -2,10 +2,14 @@ import "./App.css";
 import Article from "./components/Article/Article";
 import Aside from "./components/Aside/Aside";
 import Footer from "./components/Footer/Footer";
+import Auction from "./components/JapanCompanyVersionB/Auction";
+import CustomerCars from "./components/JapanCompanyVersionB/CustomerCars";
 
 function Main() {
   return (
     <div>
+      <Auction />
+      <CustomerCars />
       <Aside />
       <Article />
       <Footer />
