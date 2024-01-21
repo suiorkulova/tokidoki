89cedb2d8f939e150c02133efbd4bedaa9b20abb
tulobaev <talgattulobaev519>
2024-01-22 01:35:02 +0600

    add task talgat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Article from "./components/Article/Article";
+import Aside from "./components/Aside/Aside";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Aside />
+      <Article />
+    </div>
+  );
 }
 
 export default App;
